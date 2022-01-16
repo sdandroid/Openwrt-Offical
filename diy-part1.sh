456866592468953ff1b40023129b486149462897
高彬 <gaobin2496@gmail.com>
2022-01-16 12:35:22 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
+
