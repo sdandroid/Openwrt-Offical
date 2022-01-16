6f9bc62c66a9e5aaeb329fbfb38f10047b4d6468
高彬 <gaobin2496@gmail.com>
2022-01-16 13:56:13 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,3 +8,4 @@
 # Modify default IP
 #echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
+
